9f948e074de001f210ae989df586a0cd36a65d5c
Demetrius Nunes <demetriusnunes@gmail.com>
2009-01-28 22:48:14 -0200

    Completed documentation.

--- lib/clouder.rb
+++ lib/clouder.rb
@@ -7,14 +7,22 @@ require 'rest-client/lib/rest_client'
 require 'clouder/entity'  
 require 'clouder/rest'  
 
+# The Clouder module holds global server-wide functions.
 module Clouder
   VERSION = '0.0.1'
 
+  # Returns an array of URIs of the resources exposed by
+  # the CloudKit server at the +uri+.
+  #
+  #  Clouder.collection("http://localhost:9292")
+  #  => [ "notes", "comments" ]
   def Clouder.collections(uri)
     uris = Rest.get(File.join(uri, "cloudkit-meta"))["uris"]
     uris.map { |uri| uri.split("/").last }
   end
 
+  # Makes a HEAD request to the +uri+ and returns a hash
+  # of headers contained in the response.
   def Clouder.head(uri)
     Rest.custom(:head, uri)
     Rest.last_response.to_hash
--- lib/clouder/entity.rb
+++ lib/clouder/entity.rb
@@ -2,31 +2,71 @@ require 'ostruct'
 require 'time'
 
 module Clouder
+  # This is the base class to be used when accessing resources
+  # in a CloudKit server. It contains all the basic persistence
+  # methods and attributes. See the entity_spec.rb file for sample usages.
+  #
+  # A Note class should be declared as follows:
+  #   class Note < Clouder::Entity
+  #     uri "http://localhost:9292/notes"
+  #   end
   class Entity < OpenStruct
   
     class << self
-      
+
+      # If +address+ is passed, sets the URI for the target class.
+      # If nothing is passed, returns the current URI for the target class.
+      #
+      #   Note.uri "http://localhost:8989/notes" # changes the old URI
+      #   Note.uri # => "http://localhost:8989/notes"
       def uri(address = nil)
         address ? @uri = address : @uri
       end
-      
+
+      # If +options+ is nil, returns an array containing all URIs 
+      # of existing objects for this class. Sort order is from the 
+      # most recent to the oldest.
+      #
+      # For other results, +options+ can be:
+      # [:resolved] If +true+, returns full objects instead of URIs.
+      # [:offset] A positive integer, starting at 0, offsetting the result.
+      # [:limit] A positive integer, limiting the results.
+      #
+      # All options can be combined.
+      #
+      #   Note.all 
+      #   Note.all(:resolved => true)
+      #   Note.all(:offset => 20, :limit => 10)
+      #   Note.all(:resolved => true, :limit => 20, :offset => 10)
       def all(options = {})
         uri = options[:resolved] ? File.join(@uri, "_resolved") : @uri
         result = Rest.get(Rest.paramify_url(uri, options))
         options[:resolved] ? result["documents"].map { |d| new(d) } : result["uris"]
       end
       
-      def create(hsh)
-        obj = self.new(hsh)
+      # Creates and saves an object with the attributes and values
+      # passed as a hash. Returns the newly created object.
+      #
+      #   note = Note.create(:text => "My note", :author => "John Doe")
+      def create(hsh = {})
+        obj = self.new(hsh || {})
         obj.save   
         obj
       end
-      
+
+      # Returns an array of allowed HTTP methods to be requested at
+      # +uri+. If +uri+ is nil, the class URI is queried.
+      #
+      #  Note.options # => [ "GET", "HEAD", "POST", "OPTIONS" ]
       def options(uri = self.uri)
         Rest.custom(:options, uri)
         Rest.last_response["Allow"].to_s.split(",").map { |s| s.strip }
       end
 
+      # Extracts object ids from absolute or partial URIs.
+      #
+      #   Note.id_from_uri("http://localhost:9292/notes/ce655c90-cf09-012b-cd41-0016cb91f13d") 
+      #   => "ce655c90-cf09-012b-cd41-0016cb91f13d" 
       def id_from_uri(uri)
         id = URI.parse(uri)
         # /notes/abc
@@ -36,7 +76,14 @@ module Clouder
           id.to_s
         end
       end
-      
+
+      # Composes a full URI from an object id or partial URI.
+      #
+      #   Note.uri_from_id("/notes/ce655c90-cf09-012b-cd41-0016cb91f13d") 
+      #   => "http://localhost:9292/notes/ce655c90-cf09-012b-cd41-0016cb91f13d" 
+      #
+      #   Note.uri_from_id("ce655c90-cf09-012b-cd41-0016cb91f13d") 
+      #   => "http://localhost:9292/notes/ce655c90-cf09-012b-cd41-0016cb91f13d" 
       def uri_from_id(id)
         url = URI.parse(id)
         if url.absolute?
@@ -54,16 +101,37 @@ module Clouder
       
     end
 
-    attr_accessor :id, :etag, :last_modified
-    
+    # Unique object id - not an URI, just a UUID
+    attr_reader :id
+
+    # ETag, a UUID
+    attr_reader :etag
+
+    # Last modified timestamp
+    attr_reader :last_modified
+
+    # Full URI for the object
+    #   => "http://localhost:9292/notes/ce655c90-cf09-012b-cd41-0016cb91f13d" 
     def uri
       @uri ||= self.class.uri_from_id(id) if id
     end
-    
+
+    # Partial URI for the object (without the protocol, hostname)
+    #   => "/notes/ce655c90-cf09-012b-cd41-0016cb91f13d" 
     def path
       URI.parse(uri).path
     end
-    
+
+    # Constructs a new, unsaved object OR retrieves an existing object. 
+    # According to +id_or_attributes+, which can be:
+    #
+    # [nil] Initializes a new, unsaved, empty object
+    # [Hash of attributes and values] Initializes a new, unsaved object with attributes and values set
+    # [String with a URI, path or ID] Retrieves the corresponding object if exists
+    #
+    #  note = Note.new # => new, empty
+    #  note = Note.new(:text => "Ready note", :author => "Myself") # => new, with attributes set
+    #  note = Note.new("ce655c90-cf09-012b-cd41-0016cb91f13d") # => existing object
     def initialize(id_or_attributes = nil)
       @id, @etag, @last_modified, @deleted = nil
 
@@ -75,6 +143,13 @@ module Clouder
       end
     end
 
+    # Saves a new or existing object. If the object already exists,
+    # then its etag should match the etag in the database, otherwise
+    # the operation fails.
+    #
+    # Returns +true+ if save was successful, +false+ otherwise.
+    #  note = Note.new(:text => "Ready note", :author => "Myself")
+    #  note.save # => true
     def save
       result = new? ? Rest.post(collection_uri, @table) : Rest.put(uri, @table, "If-Match" => etag)
       @id, @etag, @last_modified = result.values_at("uri", "etag", "last_modified")
@@ -85,6 +160,12 @@ module Clouder
       false
     end    
 
+    # Deletes an existing object. Its etag should match the etag in 
+    # the database, otherwise the operation fails.
+    #
+    # Returns +true+ if save was successful, +false+ otherwise.
+    #  note = Note.new("ce655c90-cf09-012b-cd41-0016cb91f13d")
+    #  note.delete # => true
     def delete
       Rest.delete uri, "If-Match" => etag
       @deleted = true
@@ -94,8 +175,20 @@ module Clouder
       false
     end
 
+    # +true+ if object was not saved yet, +false+ otherwise.
+    def new?
+      @uri == nil and @etag == nil and @last_modified == nil
+    end
+
+    # +true+ if object was deleted, +false+ otherwise.
     def deleted?; @deleted end
     
+    # Retrieves older versions of the object. Sort order is from the
+    # current version to the oldest one.
+    # The +options+ parameter works as in +all+.
+    #
+    #  note = Note.new("ce655c90-cf09-012b-cd41-0016cb91f13d")
+    #  older_versions = note.versions(:resolved, :limit => 3)
     def versions(options = {})
       if uri
         url = File.join(uri, "versions")
@@ -115,15 +208,16 @@ module Clouder
         []
       end
     end
-    
+
+    # Returns an array of allowed HTTP methods to be requested for
+    # this object.
+    #
+    #  note = Note.new("ce655c90-cf09-012b-cd41-0016cb91f13d") # => existing object
+    #  note.options # => [ "DELETE", "GET", "HEAD", "PUT", "OPTIONS" ]
     def options
       self.class.options(uri)
     end
     
-    def new?
-      @uri == nil and @etag == nil and @last_modified == nil
-    end
-              
     def inspect
       "#<#{self.class.name} uri=#{uri}, id=#{id}, etag=#{@etag}, last_modified=#{@last_modified}, #{@table.inspect}>"
     end
